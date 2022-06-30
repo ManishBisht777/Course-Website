e8a091d5ea3871dc38e2ca07b8cc5164387bce32
manish <Manishbisht9711@gmail.com>
2022-06-30 17:36:32 +0530

    navbar responsive done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,40 @@
-import React from "react";
+import { React, useState } from "react";
 import "./navbar.css";
 import { BsBox } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+  const [isActive, setActive] = useState(false);
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
   return (
     <div className="navbar">
+      <div className={isActive ? "toggle open" : "toggle close"}>
+        <div className="hamburger" onClick={toggleClass}>
+          <GiHamburgerMenu />
+        </div>
+        <ul>
+          <div className="logoopen">
+            <BsBox size={30} />
+            scrumverse
+          </div>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Courses</a>
+          </li>
+          <li>
+            <a href="/">Blog</a>
+          </li>
+        </ul>
+      </div>
       <div className="logo">
         <BsBox size={30} />
         <p>Scrumverse</p>
@@ -25,7 +55,6 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-
       <div className="auth">
         <button className="button">login</button>
         <button className="button button_active">signup</button>
